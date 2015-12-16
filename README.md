56cbc9e342efd9b0888862fcfdee5384f7f73aa3
shenh10 <thushenhan@gmail.com>
2015-12-16 22:12:47 +0800

    beta 1.0

---
+++ README.md
@@ -0,0 +1,71 @@
+火车票订票助手(python版)
+===================================
+### 关于该小工具
+该脚本适用于12306刷票，无图形界面，需要安装python。适合Mac与Linux用户及对python有一定了解的用户(Windows下安装python也可)。脚本有一定安装需求，请先配置好环境再运行此脚本。本脚本无法绕过验证码输入，需要用户手动输入验证码。具体功能见下节
+
+### 功能介绍
+- 支持多车站(起始、到达) ，脚本将自动查询各个起始终点站的排列组合
+- 多日期(可列举各个日期或日期范围，无上限)
+- 支持席别选择、车次选择
+- 支持学生票
+- 支持铃声提醒
+- 支持多个乘车人
+- 支持失败次数超过阈值放弃该车次
+- 刷票间隔约1s
+- 支持自动提交
+### 安装条件
+- [Chrome浏览器](https://www.google.com/chrome/browser/desktop/index.html)
+- [Chrome Driver](http://chromedriver.storage.googleapis.com/index.html?path=2.20/)
+- python
+- splinter
+- pygame
+- configparser
+
+### 安装方法
+- 网上下载对应操作系统的Chrome浏览器并安装
+- 安装Chrome Driver
+- 安装python(Mac 与Linux一般已预装)
+- 安装pip
+- 安装依赖包
+``` bash
+pip install splinter
+pip install pygame
+pip install configparser
+```
+
+### 使用方法
+打开terminal，切换到脚本所在目录
+``` bash
+cd {YOUR_PATH_TO_THE_SCRIPT}
+```
+从模板拷贝一份设置文件
+``` bash
+cp conf.ini.template conf.ini
+```
+在conf.ini中设置你的用户名密码，根据注释设定对应参数；此处最重要的是获取你的起始到达站的cookie值,并填在[STATIONCOOKIE]区域下，模板中有广州和北京为示例。获取方法：打开12306 票查询界面，输入你的起始终点站与日期，点击查询。
+在Chrome中右键, 选择Inspect，Chrome将弹出开发者工具。
+选择Resources > Cookies > www.12306.cn
+对应'_jc_save_fromStation'和'_jc_save_toStation'的值就是你的起始/终点站的cookie值，将其以‘站名=cookie值’的形式填在[STATIONCOOKIE]区域
+安装好上述依赖之后，运行脚本
+``` bash
+python crawler.py
+```
+浏览器将自动跳转到登陆界面（用户名密码已填好），此时脚本停在了调试暂停状态
+``` bash
+(pdb)
+```
+在Chrome中输入验证码点击登录, 脚本输入c
+``` bash
+(pdb)c
+```
+用户便可坐等脚本自动刷新了。若刷到了余票，浏览器自动跳转到提交界面，只需输入验证码点提交即可。
+<br>
+脚本界面此时会显示‘是否抢票成功（Y／N）’，若用户此时已抢到票，输入Y，程序退出；否则输入N，浏览器自动返回查询界面继续刷票。
+### 退出程序
+按Ctrl－C 退出程序（浏览器窗口随之退出）
+### To do
+- 包装程序
+- 用户界面
+- 多平台可移植
+### 联系我
+若有仍和疑问、建议或批评，可以给[我](thushenhan@gmail.com)发信。
---
+++ crawler.py
@@ -0,0 +1,261 @@
+# -*- coding: utf-8 -*-
+import pdb
+import time
+import pygame
+import sys
+import configparser
+from splinter.browser import Browser
+from datetime import datetime, timedelta
+
+class Ticket(object):
+    def __init__(self, config_file):
+        ## config parser setting
+        self.config_file = config_file
+        self.settings = configparser.ConfigParser()
+        self.settings._interpolation = configparser.ExtendedInterpolation()
+        self.settings.read(self.config_file)
+        ## environment setting
+        self.brower='chrome'
+        self.b = Browser(driver_name=self.brower) 
+        self.station={}
+        self.url = "https://kyfw.12306.cn/otn/leftTicket/init"
+        # 席别类型(对应列标号)
+        self.ticket_index = [
+                            '',
+                            u'商务座',
+                            u'特等座',
+                            u'一等座', 
+                            u'二等座',
+                            u'高级软卧',
+                            u'软卧',
+                            u'硬卧',
+                            u'软座',
+                            u'硬座',
+                            u'无座'
+                            ]
+        self.username = ''
+        self.password = ''
+        self.date_format='%Y-%m-%d'
+        self.tolerance = -1
+        self.blacklist = {}
+        self.isStudent = False
+        self.success = 0
+        self.find_ticket = 0
+        self.config_parser()
+        self.playmusic = False
+        self.count = 0
+
+    def ConfigSectionMap(self,section):
+            dict1 = {}
+            options = self.settings.options(section)
+            for option in options:
+                try:
+                    dict1[option] = self.settings.get(section, option)
+                    if dict1[option] == -1:
+                        DebugPrint("skip: %s" % option)
+                except:
+                        print("exception on %s!" % option)
+                        dict1[option] = None
+            return dict1
+    def daterange(self, start_date, end_date):
+        for n in range(int ((end_date - start_date).days)):
+            yield start_date + timedelta(n) 
+
+    def config_parser(self):
+        self.retrieve_station_dict()
+        self.retrieve_book_options()
+        
+    def retrieve_station_dict(self):
+        dict_helper=self.ConfigSectionMap('STATIONCOOKIE')
+        for name, value in dict_helper.iteritems():
+            self.station[name]=value
+    def retrieve_book_options(self):
+        login_info=self.ConfigSectionMap('GLOBAL')
+        self.username = login_info['username'] 
+        self.password = login_info['password'] 
+        self.brower = login_info['browser']
+        book_settings = self.ConfigSectionMap('TICKET')
+        self.fromStation = book_settings['from_station'].split(',')
+        self.toStation = book_settings['to_station'].split(',')
+        self.trains = book_settings['trains'].split(',')
+        self.ticket_type = book_settings['ticket_type'].split(',')
+        rangeQuery = book_settings['range_query']
+        if rangeQuery == 'Y':
+            date = book_settings['date'].split(',')
+            if len(date) < 2:
+                return
+            else: 
+                start_date = datetime.strptime(date[0].strip(),self.date_format) 
+                end_date = datetime.strptime(date[1].strip(),self.date_format) 
+                if end_time < start_time:
+                    return
+                for single_date in self.daterange(start_date, end_date): 
+                    self.date.append(single_date.strftime(self.date_format))
+        else:
+            self.date = book_settings['date'].split(',')
+        if book_settings['student'] == 'Y':
+            self.isStudent = True
+        self.tolerance = book_settings['tolerance']
+        self.people = book_settings['people'].split(',')
+        if book_settings['alarm'] == 'Y':
+            self.playmusic = True
+
+    def login(self):
+        self.b.visit(self.url)
+        self.b.find_by_text(u"登录").click()
+        self.b.fill("loginUserDTO.user_name",self.username)
+        self.b.fill("userDTO.password",self.password)
+        pdb.set_trace()
+    
+    def page_has_loaded(self):
+        page_state = self.b.evaluate_script("document.readyState")
+        return page_state == 'complete'
+
+    def switch_to_order_page(self):
+        order = []
+        while len(order) == 0:
+            order = self.b.find_by_text(u"车票预订")
+        order[0].click()
+
+    def checkTicket(self, date, fromStation, toStation):
+        print 'date: %s, from %s, to %s'%(date, fromStation, toStation)
+        self.b.cookies.add({"_jc_save_fromDate":date})
+        ## From : Guangzhou
+        self.b.cookies.add({"_jc_save_fromStation":self.station[fromStation]})
+        ## To : Huaihua
+        self.b.cookies.add({"_jc_save_toStation":self.station[toStation]})
+        self.b.cookies.all()
+        self.b.reload()
+        if self.isStudent:
+            self.b.find_by_text(u'学生').click()
+        self.b.find_by_text(u"查询").click()
+        all_trains = []
+        while self.page_has_loaded() == False:
+            continue
+        while len(all_trains) == 0:
+            all_trains = self.b.find_by_id('queryLeftTable').find_by_tag('tr')
+        for k, train in enumerate(all_trains):
+            tds = train.find_by_tag('td')
+            if tds and len(tds) >= 10:
+                has_ticket= tds.last.find_by_tag('a')
+                if len(has_ticket) != 0:
+                    if k + 1 < len(all_trains):
+                        this_train = all_trains[k+1]['datatran']
+                        if len(self.trains) != 0 and this_train not in self.trains:
+                            continue
+                        if self.tolerance != -1 and this_train in self.blacklist and self.blacklist[this_train] > self.tolerance:
+                            print u"%s 失败 %d 次, 跳过"%(this_train, self.blacklist[this_train])
+                            continue
+                    for cat in self.ticket_type:
+                        if cat in ticket_index:
+                            i = ticket_index.index(cat)
+                        else:
+                            return 0, ''
+                        if tds[i].text != u'无' and tds[i].text != '--':
+                            if tds[i].text != u'有':
+                                print u'%s 的 %s 有余票 %s 张!'%(this_train, cat ,tds[i].text)
+                            else:
+                                print u'%s 的 %s 有余票若干张!'%(this_train, cat ,tds[i].text)
+                            self.find_ticket = 1
+                            tds.last.click()
+                            break
+            if self.find_ticket:
+                break
+        return this_train
+
+    def book(self):
+        while self.page_has_loaded() == False:
+            continue
+        more = []
+        while len(more) == 0:
+            more = self.b.find_by_text(u"更多")
+        more[0].click()
+        person = []
+        while len(person) == 0:
+            person=b.find_by_text(persons[0])
+        for person in self.people:
+            b.find_by_text(person).click()
+        return 1
+
+    def ring(self):
+        pygame.mixer.pre_init(64000, -16, 2, 4096)
+        pygame.init()
+        pygame.display.init()
+        screen=pygame.display.set_mode([300,300])
+        #pygame.display.flip()
+        pygame.time.delay(1000)#等待1秒让mixer完成初始化
+        tracker=pygame.mixer.music.load("sound.ogg")
+        #track = pygame.mixer.music.load("sound.ogg")
+        pygame.mixer.music.play()
+        # while pygame.mixer.music.get_busy():
+        #pygame.time.Clock().tick(10)
+        running = True
+        img=pygame.image.load("img.jpg")
+        while running:
+            screen.blit(img,(0,0))
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type==pygame.QUIT:
+                    running = False
+        pygame.quit ()
+        return 1
+    def executor(self):
+        self.login()
+        self.switch_to_order_page()
+        while self.success == 0:
+            self.find_ticket = 0
+            while self.find_ticket == 0:
+                for date in self.date:
+                    try:
+                        self.count += 1
+                        print "Try %d times" % self.count
+                        for fromStation in self.fromStation:
+                            for toStation in self.toStation:
+                                this_train = self.checkTicket(date, fromStation, toStation)
+                                if self.find_ticket:
+                                    break
+                            if self.find_ticket:
+                                break
+                    except KeyboardInterrupt:
+                        self.b.find_by_text(u'退出').click()
+                        sys.exit()
+                    except:
+                        continue
+            # book ticket for target people
+            self.find_ticket = 0
+            while self.find_ticket == 0:
+                try:
+                    self.find_ticket = self.book() 
+                except KeyboardInterrupt:
+                    self.b.find_by_text(u'退出').click()
+                    sys.exit()
+                except:
+                    continue
+            if self.playmusic:
+                self.ring()
+            print "订票成功了吗?(Y/N)"
+            input_var = ''
+            while input_var == '':
+                input_var= sys.stdin.read(1)
+                if input_var == 'Y' or input_var == 'y':
+                    self.success = 1
+                elif input_var == 'N' or input_var == 'n':
+                    if this_train in self.blacklist:
+                        self.blacklist[this_train] += 1
+                    else:
+                        self.blacklist[this_train] = 1
+                    print u"%s 失败 %d 次"%(this_train, self.blacklist[this_train])
+                    self.b.back()
+                    self.b.reload()
+                else:
+                    input_var = ''
+                    continue
+        self.b.find_by_text(u'退出').click()
+
+if __name__ == '__main__':
+    ##start login
+    ticket_theif = Ticket('./conf.ini') 
+    try:
+        ticket_theif.executor() 
+    except KeyboardInterrupt:
+        sys.exit()
