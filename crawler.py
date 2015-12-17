d40f3307c77d970d49cb10af2ee722d429da4e87
shenh10 <thushenhan@gmail.com>
2015-12-17 20:38:56 +0800

    Repair several bugs, testing all functionalities

--- crawler.py
+++ crawler.py
@@ -38,6 +38,7 @@ class Ticket(object):
         self.date_format='%Y-%m-%d'
         self.tolerance = -1
         self.blacklist = {}
+        self.date = []
         self.isStudent = False
         self.success = 0
         self.find_ticket = 0
@@ -62,13 +63,16 @@ class Ticket(object):
             yield start_date + timedelta(n) 
 
     def config_parser(self):
-        self.retrieve_station_dict()
-        self.retrieve_book_options()
+        if self.retrieve_station_dict() == -1:
+            sys.exit()
+        if self.retrieve_book_options() == -1:
+            sys.exit()
         
     def retrieve_station_dict(self):
         dict_helper=self.ConfigSectionMap('STATIONCOOKIE')
         for name, value in dict_helper.iteritems():
             self.station[name]=value
+
     def retrieve_book_options(self):
         login_info=self.ConfigSectionMap('GLOBAL')
         self.username = login_info['username'] 
@@ -77,25 +81,31 @@ class Ticket(object):
         book_settings = self.ConfigSectionMap('TICKET')
         self.fromStation = book_settings['from_station'].split(',')
         self.toStation = book_settings['to_station'].split(',')
-        self.trains = book_settings['trains'].split(',')
+        trains = book_settings['trains'].split(',')
+        if len(trains) == 1 and trains[0].strip() == '':
+            self.trains = []
+        else:
+            self.trains =  trains
         self.ticket_type = book_settings['ticket_type'].split(',')
         rangeQuery = book_settings['range_query']
         if rangeQuery == 'Y':
             date = book_settings['date'].split(',')
             if len(date) < 2:
-                return
+                print "未设置正确的起至时间"
+                return -1
             else: 
                 start_date = datetime.strptime(date[0].strip(),self.date_format) 
                 end_date = datetime.strptime(date[1].strip(),self.date_format) 
-                if end_time < start_time:
-                    return
+                if end_date < start_date:
+                    print "查询截止日期不可大于开始日期!"
+                    return -1
                 for single_date in self.daterange(start_date, end_date): 
                     self.date.append(single_date.strftime(self.date_format))
         else:
             self.date = book_settings['date'].split(',')
         if book_settings['student'] == 'Y':
             self.isStudent = True
-        self.tolerance = book_settings['tolerance']
+        self.tolerance = int(book_settings['tolerance'])
         self.people = book_settings['people'].split(',')
         if book_settings['alarm'] == 'Y':
             self.playmusic = True
@@ -143,19 +153,20 @@ class Ticket(object):
                         this_train = all_trains[k+1]['datatran']
                         if len(self.trains) != 0 and this_train not in self.trains:
                             continue
-                        if self.tolerance != -1 and this_train in self.blacklist and self.blacklist[this_train] > self.tolerance:
+                        if self.tolerance != -1 and this_train in self.blacklist and self.blacklist[this_train] >= self.tolerance:
                             print u"%s 失败 %d 次, 跳过"%(this_train, self.blacklist[this_train])
                             continue
                     for cat in self.ticket_type:
-                        if cat in ticket_index:
-                            i = ticket_index.index(cat)
+                        if cat in self.ticket_index:
+                            i = self.ticket_index.index(cat)
                         else:
+                            print '无效的席别信息'
                             return 0, ''
                         if tds[i].text != u'无' and tds[i].text != '--':
                             if tds[i].text != u'有':
                                 print u'%s 的 %s 有余票 %s 张!'%(this_train, cat ,tds[i].text)
                             else:
-                                print u'%s 的 %s 有余票若干张!'%(this_train, cat ,tds[i].text)
+                                print u'%s 的 %s 有余票若干张!'%(this_train, cat)
                             self.find_ticket = 1
                             tds.last.click()
                             break
@@ -166,16 +177,22 @@ class Ticket(object):
     def book(self):
         while self.page_has_loaded() == False:
             continue
+        if len(self.people) == 0:
+            print '没有选择乘车人!'
+            return 1
         more = []
         while len(more) == 0:
             more = self.b.find_by_text(u"更多")
         more[0].click()
         person = []
+        people = self.people
         while len(person) == 0:
-            person=b.find_by_text(persons[0])
-        for person in self.people:
-            b.find_by_text(person).click()
-        return 1
+            person=self.b.find_by_xpath('//ul[@id="normal_passenger_id"]/li/label[contains(text(),"%s")]'%people[0])
+        for p in people:
+            self.b.find_by_xpath('//ul[@id="normal_passenger_id"]/li/label[contains(text(),"%s")]'%p).click()
+            if self.b.find_by_xpath('//div[@id="dialog_xsertcj"]').visible:
+                self.b.find_by_xpath('//div[@id="dialog_xsertcj"]/div/div/div/a[text()="确认"]').click()
+            return 1
 
     def ring(self):
         pygame.mixer.pre_init(64000, -16, 2, 4096)
@@ -216,6 +233,8 @@ class Ticket(object):
                                     break
                             if self.find_ticket:
                                 break
+                        if self.find_ticket:
+                            break
                     except KeyboardInterrupt:
                         self.b.find_by_text(u'退出').click()
                         sys.exit()
