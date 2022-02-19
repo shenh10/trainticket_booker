be2efbaf46f7f9bf6c00258ec0af521930803a49
lpb-cxs <62924426+lpb-cxs@users.noreply.github.com>
2022-02-19 19:58:06 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -13,7 +13,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
-
+from selenium.common.exceptions import TimeoutException
 from datetime import datetime, timedelta
 import pdb
 
